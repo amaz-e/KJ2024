3ea84fc8456ebd1236dad77c2179515162837c67
Adam <timmlion.istiris@gmail.com>
2024-01-28 00:47:23 +0100

    asdasd

--- backend/MemeBE/hubs/GameHub.cs
+++ backend/MemeBE/hubs/GameHub.cs
@@ -108,7 +108,7 @@ public class GameHub : Hub
             
             foreach (var player in room.Players.Values)
             {
-                //TODO Zainicjuj gre dla kaźdego gracza
+                // Zainicjuj gre dla kaźdego gracza
                 for (int i = 0; i < 5; i++)
                 {
                     CardDrawn(player, room);
@@ -139,7 +139,7 @@ public class GameHub : Hub
         {
             await Clients.Caller.SendAsync("RoomError", "No no no no - You can't make moves in this room");
         }
-        else if (room.ActivePlayer.ConnectionID.Equals(room.GetPlayerByConnID(Context.ConnectionId).Nick))
+        else if (room.ActivePlayer.ConnectionID.Equals(Context.ConnectionId))
         {
             // tutaj jest aktywny gracz w odpowiednim pokoju
             // Wyliczanie akcjki
@@ -183,8 +183,6 @@ public class GameHub : Hub
                     
                 }
             }
-
-            EndTurn(room);
         }
     }
     public async Task EndTurn(Room room)
@@ -243,7 +241,6 @@ public class GameHub : Hub
                 await Clients.Group(room.RoomId)
                     .SendAsync("ReceiveServerRoomMessage", GetLaughMessage(player.Nick, laughValue));
             }
-            
         }
         else // Single player
         {
@@ -255,7 +252,7 @@ public class GameHub : Hub
             await Clients.Group(room.RoomId)
                 .SendAsync("ReceiveServerRoomMessage", GetLaughMessage(targetPlayer.Nick, laughValue));
         }
-        
+        EndTurn(room);
     }
 
 
@@ -284,41 +281,51 @@ public class GameHub : Hub
             await Clients.Group(room.RoomId)
                 .SendAsync("ReceiveServerRoomMessage", GetGrumpyMessage(targetPlayer.Nick, grumpyValue));
         }
+        EndTurn(room);
     }
 
-    public void PlacePersistentCard(Room room, Card card, String targetNick)
+    public async Task PlacePersistentCard(Room room, Card card, String targetNick)
     {
         var player = room.GetPlayerByNick(targetNick);
         if (player.AddCardToPersistentSlot(card).Sucess)
         {
-            //emit   
+            await Clients.Client(player.ConnectionID).SendAsync("PlacedPersistant",card.DeckId, card.URL, card.Target);
+            var otherPlayers = GetOtherPlayers(room, player.ConnectionID).Select(player => player.Nick).ToList();
+            foreach (var nick in otherPlayers)
+            {
+                await Clients.Client(room.GetPlayerByNick(nick).ConnectionID).SendAsync("OtherPlacedPersistant",card.DeckId, card.URL, card.Target);
+            }
+            await Clients.OthersInGroup(room.RoomId).SendAsync("OtherPlacedPersistant",card.DeckId, card.URL, player.Nick);
+            await Clients.Group(room.RoomId)
+                .SendAsync("ReceiveServerRoomMessage", GetPersistantMessage(player.Nick, card.EffectList[0].EffectName));
         }
+        EndTurn(room);
     }
     private string GetLaughMessage(string playerNick, int laughValue)
     {
         Random rnd = new Random();
         string[] laughMessages = new string[]
         {
-            "Hold onto your funny bones! " + playerNick + " has just boosted the laugh-o-meter with an extra " +
-            laughValue + " HAHA points!",
-            "Alert! " + playerNick + " is on a chuckle charge, racking up another " + laughValue +
-            " HAHA points to their giggle bank!",
-            "Whoa, " + playerNick + " is serving up some serious hilarity with an additional " + laughValue +
-            " HAHA points! Comedy gold!",
-            "What's that sound? It's " + playerNick + " laughing all the way to the HAHA hall of fame with " +
-            laughValue + " more points of pure mirth!",
-            "Breaking news: " + playerNick + "'s laughter level just skyrocketed by " + laughValue +
-            " HAHA points. The fun-o-sphere is off the charts!",
-            "Gather 'round, folks! Witness " + playerNick + " sprinkle a dash of chuckles, adding " + laughValue +
-            " HAHA points to their giggle tally!",
-            "It's no joke! " + playerNick + " is officially a laughter legend, piling on an extra " + laughValue +
-            " HAHA points to their chuckle stash!",
-            "Behold! " + playerNick + " has unleashed a laughnado, swirling up " + laughValue +
-            " HAHA points in a frenzy of funniness!",
-            "Can you feel the ground shaking? That's just " + playerNick + " laughing up an earthquake with another " +
-            laughValue + " HAHA points on the Richter scale!",
-            "Quick, get the giggle goggles! " + playerNick + " is blinding us with brilliance, bagging " + laughValue +
-            " more HAHA points in a spectacular show of snickers!"
+            "Hold onto your funny bones! <b>" + playerNick + "</b> has just boosted the laugh-o-meter with an extra <b>" +
+            laughValue + "</b> HAHA points!",
+            "Alert! <b>" + playerNick + "</b> is on a chuckle charge, racking up another <b>" + laughValue +
+            "</b> HAHA points to their giggle bank!",
+            "Whoa, <b>" + playerNick + "</b> is serving up some serious hilarity with an additional <b>" + laughValue +
+            "</b> HAHA points! Comedy gold!",
+            "What's that sound? It's <b>" + playerNick + "</b> laughing all the way to the HAHA hall of fame with <b>" +
+            laughValue + "</b> more points of pure mirth!",
+            "Breaking news: <b>" + playerNick + "</b>'s laughter level just skyrocketed by <b>" + laughValue +
+            "</b> HAHA points. The fun-o-sphere is off the charts!",
+            "Gather 'round, folks! Witness <b>" + playerNick + "</b> sprinkle a dash of chuckles, adding <b>" + laughValue +
+            "</b> HAHA points to their giggle tally!",
+            "It's no joke! <b>" + playerNick + "</b> is officially a laughter legend, piling on an extra <b>" + laughValue +
+            "</b> HAHA points to their chuckle stash!",
+            "Behold! <b>" + playerNick + "</b> has unleashed a laughnado, swirling up <b>" + laughValue +
+            "</b> HAHA points in a frenzy of funniness!",
+            "Can you feel the ground shaking? That's just <b>" + playerNick + "</b> laughing up an earthquake with another <b>" +
+            laughValue + "</b> HAHA points on the Richter scale!",
+            "Quick, get the giggle goggles! <b>" + playerNick + "</b> is blinding us with brilliance, bagging <b>" + laughValue +
+            "</b> more HAHA points in a spectacular show of snickers!"
         };
 
         int randomIndex = rnd.Next(laughMessages.Length);
@@ -329,20 +336,68 @@ public class GameHub : Hub
         Random rnd = new Random();
         string[] grumpyMessages = new string[]
         {
-            "Uh oh! " + playerNick + " just unleashed a grump-storm, draining " + grumpyValue + " joy points from the room. Cheer up, buttercup!",
-            "Alert the mood squad! " + playerNick + " is having a no-smiles spell, dropping " + grumpyValue + " happiness points like hot potatoes!",
-            "Gloomy alert! " + playerNick + " has just cast a little cloud of frowns, sapping " + grumpyValue + " cheer points from the happiness meter!",
-            "Who turned down the joy tunes? " + playerNick + " has, with a grumpyValue-sized dip in the vibe vibes!",
-            "Hold the chuckles, " + playerNick + " is on a serious spree, swiping " + grumpyValue + " giggle points from the laugh ledger!",
-            "Warning! " + playerNick + "'s smile seems to be on a brief break, taking " + grumpyValue + " glee points with it!",
-            "Looks like " + playerNick + " hit a pothole on the joyride, causing a loss of " + grumpyValue + " happy points. Time for a pick-me-up!",
-            "Is it just me, or did " + playerNick + " just start a frown-a-thon? There go " + grumpyValue + " points from the joy jar!",
-            "Notice: " + playerNick + "’s happiness meter just experienced a slight dip, with a " + grumpyValue + " drop in delight digits!",
-            "Eek! " + playerNick + " just tripped on a grumble bump, shedding " + grumpyValue + " bliss bits off their happiness score!"
+            "Uh oh! <b>" + playerNick + "</b> just unleashed a grump-storm, draining <b>" + grumpyValue + "</b> joy points from the room. Cheer up, buttercup!",
+            "Alert the mood squad! <b>" + playerNick + "</b> is having a no-smiles spell, dropping <b>" + grumpyValue + "</b> happiness points like hot potatoes!",
+            "Gloomy alert! <b>" + playerNick + "</b> has just cast a little cloud of frowns, sapping <b>" + grumpyValue + "</b> cheer points from the happiness meter!",
+            "Who turned down the joy tunes? <b>" + playerNick + "</b> has, with a grumpyValue-sized dip in the vibe vibes!",
+            "Hold the chuckles, <b>" + playerNick + "</b> is on a serious spree, swiping <b>" + grumpyValue + "</b> giggle points from the laugh ledger!",
+            "Warning! <b>" + playerNick + "</b>'s smile seems to be on a brief break, taking <b>" + grumpyValue + "</b> glee points with it!",
+            "Looks like <b>" + playerNick + "</b> hit a pothole on the joyride, causing a loss of <b>" + grumpyValue + "</b> happy points. Time for a pick-me-up!",
+            "Is it just me, or did <b>" + playerNick + "</b> just start a frown-a-thon? There go <b>" + grumpyValue + "</b> points from the joy jar!",
+            "Notice: <b>" + playerNick + "</b>’s happiness meter just experienced a slight dip, with a <b>" + grumpyValue + "</b> drop in delight digits!",
+            "Eek! <b>" + playerNick + "</b> just tripped on a grumble bump, shedding <b>" + grumpyValue + "</b> bliss bits off their happiness score!"
         };
 
         int randomIndex = rnd.Next(grumpyMessages.Length);
         return grumpyMessages[randomIndex];
+    }
+    
+    private string GetPersistantMessage(string playerNick, string effectName)
+    {
+        Random rnd = new Random();
+        string[] persistantMessage = new string[]{playerNick + " Did something completely different"};
+        if (effectName.Equals("Buff"))
+        {
+            persistantMessage = new string[]
+            {
+                "Watch out! <b>" + playerNick +
+                "</b> just played a buff card, bolstering their strength for the next round.",
+                "Keep your eyes peeled! <b>" + playerNick +
+                "</b> has just set up a powerful buff, enhancing their capabilities.",
+                "Brace yourselves, <b>" + playerNick + "</b> boosts their arsenal with a well-timed buff card.",
+                "Calculating... <b>" + playerNick +
+                "</b> plays a buff card, and their power grows silently but surely.",
+                "Anticipation builds as <b>" + playerNick +
+                "</b> activates a buff card, their strength quietly surging.",
+                "Subtle yet effective, <b>" + playerNick + "</b> places a buff card, setting the stage for a comeback.",
+                "Poised and ready, <b>" + playerNick + "</b> empowers themselves with a game-changing buff card.",
+                "Strength in stillness, <b>" + playerNick + "</b> has played a buff card, their power lying in wait.",
+                "Quiet before the storm, <b>" + playerNick +
+                "</b> lays down a buff card, their potential energy growing.",
+                "Ready for the future, <b>" + playerNick + "</b> buffs up, laying the groundwork for victory."
+            };
+        }
+        else if(effectName.Equals("Shield"))
+
+        {
+            persistantMessage = new string[]
+            {
+                "Strategic mastery! <b>" + playerNick + "</b> fortifies their defenses with an impenetrable shield card.",
+                "Shrewd move by <b>" + playerNick + "</b> as they deploy a shield card, preparing for whatever comes next!",
+                "With a flick of the wrist, <b>" + playerNick + "</b> lays down a shield card, warding off future attacks.",
+                "The tides may turn! <b>" + playerNick + "</b> casts a shield, raising their defenses to new heights.",
+                "A moment of peace as <b>" + playerNick + "</b> reinforces their position with a sturdy shield card.",
+                "Defensive stance! <b>" + playerNick + "</b> has played a shield card, securing their ground.",
+                "An aura of protection envelops <b>" + playerNick + "</b> as they play a strategic shield card.",
+                "<b>" + playerNick + "</b> casts a shield, cloaking themselves in an invisible but unbreakable barrier.",
+                "A shield card enters the field! <b>" + playerNick + "</b> is now shielded against the next onslaught.",
+                "Defense is the best offense! <b>" + playerNick + "</b> places a shield card with a knowing smile."
+
+            };
+        }
+
+        int randomIndex = rnd.Next(persistantMessage.Length);
+        return persistantMessage[randomIndex];
     }
 
     // Target = 0 - all
