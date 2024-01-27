8b9608357bada178e277623891aa559df54ae629
Lisiasty <69064684+Lisiasty-Pazurek@users.noreply.github.com>
2024-01-27 14:42:57 +0100

    Revert "Merge branch 'develop' of https://github.com/Timmlion/KJ2024 into develop"
    This reverts commit a42b8fee52441d8391edfccdafbd81c0f9c244ce, reversing
    changes made to 19d7f29c3d8ba5614e6bf32f615dd3de421d7c28.

--- backend/MemeBE/Models/Room.cs
+++ backend/MemeBE/Models/Room.cs
@@ -2,26 +2,13 @@ namespace MemeBE.Models;
 
 public class Room
 {
-    public string RoomId { get; set; }
+    public int RoomId { get; set; }
     public Dictionary<string,Player> Players { get; set; }
     public bool Locked { get; set; }
-    
-    //Cards that can be drawn
-    public Deck Deck { get; set; }
-    // Is game started
-    public bool GameStarted { get; set; } = false;
-    public string Owner { get; set; }
-    public Player ActivePlayer { get; set; }
-    public Queue<Player> PlayerQueue { get; set; }
-    
-    
-    public Room(string roomID, string owner)
+    public Room(int roomID)
     {
         RoomId = roomID;
-        Owner = owner;
-        Players = new Dictionary<string, Player>();
-
-        Deck = new Deck();
+        Players = new Dictionary<string, Player>(); 
     }
 
     public Result AddPlayer(Player player)
@@ -39,40 +26,9 @@ public class Room
         return Result.Ok("Connected to room");
     }
     
-    
-    
     public bool ContainsPlayerWithNick(string nick)
     {
         // Check if any player in the dictionary has the specified nick
         return Players.Values.Any(player => player.Nick.Equals(nick, StringComparison.OrdinalIgnoreCase));
-    }
-
-    public Player GetPlayerByConnID(string connId)
-    {
-        return Players.Values.First(player => player.ConnectionID.Equals(connId));
-    }
-
-    public void InitGame()
-    {
-        //TODO stworzenie talli
-        
-        //TODO wygenerowanie kolejki
-        Random rng = new Random();
-        PlayerQueue = new Queue<Player>(Players.Values.OrderBy(p => rng.Next()));
-        //TODO ustawienie aktywnego gracza
-        ActivePlayer = NextPlayer();
-
-    }
-    
-    public Player NextPlayer()
-    {
-        // Dequeue the player from the front of the queue.
-        var player = PlayerQueue.Dequeue();
-
-        // Immediately enqueue the player at the back of the queue.
-        PlayerQueue.Enqueue(player);
-
-        // Return the player that was processed.
-        return player;
     }
 }
