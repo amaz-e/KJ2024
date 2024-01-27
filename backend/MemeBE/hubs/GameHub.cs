6adb1ab8b4e7e0183082d394f67078a1f1582e1e
pmadry <madry.piotrek@gmail.com>
2024-01-27 01:10:35 +0100

    uch

--- backend/MemeBE/hubs/GameHub.cs
+++ backend/MemeBE/hubs/GameHub.cs
@@ -4,18 +4,23 @@ namespace MemeBE.hubs;
 
 public class GameHub : Hub
 {
+    private int nextRoomID = 1;
+    
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
 
-    public async Task CreateRoom(string PlayerName)
+    public async Task CreateRoom(string playerName)
     {
-        
+        await JoinRoom(playerName, nextRoomID);
+        nextRoomID++;
     }
 
-    public async Task JoinRoom(string PlayerName)
+    public async Task JoinRoom(string playerName, int roomID)
     {
-        
+        await Groups.AddToGroupAsync(Context.ConnectionId, nextRoomID.ToString());
+        await Clients.Caller.SendAsync("JoinedToRoom", nextRoomID);
+        await Clients.OthersInGroup(roomID.ToString()).SendAsync("NewPlayerJoinedToRoom", playerName);
     }
 }
